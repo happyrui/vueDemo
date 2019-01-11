9545f2bfb7d1045e6a81b86ce75778d8701c65eb
viviarui <zr1450995198@163.com>
2019-01-11 20:04:16 +0800

    入口文件

---
+++ README.md
@@ -0,0 +1,3 @@
+### webpack 原生构建 vue 项目
+#### 状态管理 vuex
+#### 组件库 vux / Element
---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+// import ElementUI from 'element-ui';
+// import { Button, Select } from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router'
+
+
+// 注册 全部引入
+// Vue.use(ElementUI)
+// 按需引入
+// Vue.use(Button)
+
+// 挂载
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+// new Vue({
+//   // el: '#app',
+//   render: h => h(App),
+//   data() {
+//     console.log(this)
+//     return {
+//       message: 'Hello World!'
+//     }
+//   }
+// }).$mount(root)
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  // el: '#app',
+  render: h => h(App),
+  router,
+  components: { App },
+  template: '<App/>'
+}).$mount(root)
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+
