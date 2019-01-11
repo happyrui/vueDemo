6ec18e983dab562277716d8c974a6c5e908cf8cb
viviarui <zr1450995198@163.com>
2019-01-11 20:03:28 +0800

    组件

---
+++ src/components/list.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="rowcolCenter">
+        {{ msg }}
+        <!-- <el-button>跳转详情</el-button> -->
+        <router-link to='./detail'>跳转详情</router-link>
+    </div>
+</template>
+<script>
+import { Button, Select } from 'element-ui';
+export default {
+    data(){
+        return {
+            msg: 'this is list'
+        }
+    }
+}
+</script>
+<style scoped>
+.rowcolCenter {
+    width: '100%';
+    height: '600px';
+    display: flex;
+    justify-content: center
+}
+</style>
+
+
