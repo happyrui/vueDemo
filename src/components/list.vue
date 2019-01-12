7ff8d12cc351010416acfeb2fbd74dda0629ed8b
viviarui <zr1450995198@163.com>
2019-01-12 12:43:09 +0800

    引入vue-resource请求接口，Element样式组件

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="rowcolCenter">
         {{ msg }}
-        <!-- <el-button>跳转详情</el-button> -->
+        <!-- <ul>
+            <li v-for="item in items">
+                {{item.title}}
+            </li>
+        </ul> -->
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <h2 style="line-height: 36px; color: #20A0FF">豆瓣电影排行榜</h2>
+            </div>
+            <div v-for="item in items" class="text item">
+                {{item.title}}
+            </div>
+        </el-card>
         <router-link to='./detail'>跳转详情</router-link>
     </div>
 </template>
@@ -10,8 +22,27 @@ import { Button, Select } from 'element-ui';
 export default {
     data(){
         return {
-            msg: 'this is list'
+            msg: 'this is list',
+            items: this.items
         }
+    },
+    // 周期函数
+    mounted: function(){
+        console.log(this);
+        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+            headers: {
+ 
+            },
+            emulateJSON: true
+        }).then(function(response) {
+        // 这里是处理正确的回调
+            this.items = response.body.subjects
+            // this.articles = response.data["subjects"] 也可以
+    
+        }, function(response) {
+            // 这里是处理错误的回调
+            console.log(response)
+        });
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
-// import ElementUI from 'element-ui';
+import ElementUI from 'element-ui';
 // import { Button, Select } from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import VueResource from 'vue-resource';
 import router from './router'
 
 
 // 注册 全部引入
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 // 按需引入
 // Vue.use(Button)
+
+// 注册 请求数据
+Vue.use(VueResource)
 
 // 挂载
 const root = document.createElement('div');
