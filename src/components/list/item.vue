e4931b40fa33e1b0ccfe5e194b5d982dff3a88db
viviarui <zr1450995198@163.com>
2019-01-14 16:57:02 +0800

    item  子组件

---
+++ src/components/list/item.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <el-row>
+            <input v-model="beginNum">
+            <el-button type="success" @click="add">点击求幂</el-button>
+        </el-row>
+        <el-row>
+            结果：<label>{{changeNum}}</label>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        changeNum: {
+            type: Number,
+            required: true
+        }
+    },
+    methods: {
+        add() {
+            //  函数名 与 父组件属性名一致
+            this.$emit('fuAdd', this.beginNum)
+        }
+    }
+}
+</script>
+
