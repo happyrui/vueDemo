cec3f0f3dcd918e6130096b96546ab2df578792f
viviarui <zr1450995198@163.com>
2019-01-17 17:34:15 +0800

    修改页面

--- src/components/list/index.vue
+++ src/components/list/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="rowcolCenter">
-        {{ msg }}
         <!-- <ul>
             <li v-for="item in items">
                 {{item.title}}
@@ -18,10 +17,11 @@
             </div>
         </el-card>
         <el-row>
+            <el-button type="success" @click="getData">获取</el-button>
             <el-button type="primary">
                 <router-link to='./detail'>跳转详情</router-link>
             </el-button>
-            <div>{{reversedMessage}}</div>
+            <!-- <div>{{reversedMessage}}</div>
             <input v-model="text" />
             <p>{{text}}</p>
             <select v-model="selected">
@@ -29,69 +29,74 @@
                     {{ option.text }}
                 </option>
             </select>
-            <span>Selected: {{ selected }}</span>
+            <span>Selected: {{ selected }}</span> -->
             <!-- 向一个组件传递内容 -->
-            <Item 
+            <!-- <Item 
                 @fuAdd="addFun" 
                 :changeNum = "changeNum"
             >
-             sffdfds</Item>
+             sffdfds</Item> -->
         </el-row>
     </div>
 </template>
 <script>
 import Item from './item';
-console.log(Item)
+import { mapState, mapActions  } from 'vuex';
 export default {
     // 引入 子组件
     components: {
         Item
     },
+    // 使用Vuex时，之后在页面vue中只写 生命周期钩子，计算属性，方法等
+    // 对于 初始化data，actions，getters，mutations 已经更改数据源 都放在对应的 vuex文件中
     data(){
         return {
-            msg: 'this is list',
-            items: this.items,
-            text:'',
-            selected: 'A',
-            options: [
-                { text: 'One', value: 'A' },
-                { text: 'Two', value: 'B' },
-                { text: 'Three', value: 'C' }
-            ],
-            changeNum: 0
+            // items: this.$store.getters.itemsaaa //获取store中state的数据
         }
     },
     // 计算属性 复杂逻辑时使用
-    computed: {
+    computed: mapState({
         // 匿名函数作为属性reversedMessage 的getter函数
         // 计算属性的 getter
-        reversedMessage: function () {
-        // `this` 指向 vm 实例
-            return this.msg.split('').reverse().join('')
+        // reversedMessage: function (state) {
+        //     let [ a, b, c, d ] = ['1', '2', '3', '4'];
+        //     console.log(a,b,c,d)
+        //     // `this` 指向 vm 实例
+        //     console.log(state)
+        //     return state.list.msg.split('').reverse().join('')
+        // },
+        // count: function(state) {
+        //     console.log(this.$store) 
+        //     return state.list.count++
+        // },
+        // message: function(state) {
+        //     return state.list.message
+        // }
+        items: function(state) {
+            // 使用modules时，需要使用 state.moduleA.xx 来取值
+            return state.list.items
         }
-    },
+    }),
     // 周期函数
+    // 所有的生命周期钩子自动绑定 this 上下文到实例中，因此你可以访问数据，对属性和方法进行运算
+    // 不能使用 箭头函数 定义生命周期函数 这是因为箭头函数绑定了父上下文，因此 this 与你期待的 Vue 实例不同，
     mounted: function(){
-        console.log(this);
-        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
-            headers: {},
-            emulateJSON: true
-        }).then(function(response) {
-        // 这里是处理正确的回调
-            this.items = response.body.subjects
-            // this.articles = response.data["subjects"] 也可以
-    
-        }, function(response) {
-            // 这里是处理错误的回调
-            console.log(response)
-        });
+        // 触发action
+        this.$store.dispatch('actionItems'); // 使用actions 异步触发
+        // 提交 mutations
+        // this.$store.commit('getItems') // 使用 mutations 同步触发
+        // this.getData();
+        console.log(this)
     },
-    methods: {
+    methods: { 
+        ...mapActions({
         // 父组件调用的方法
-        addFun(i) {
-            this.changeNum =  i*i;
-        }
-    }
+        // addFun(i) {
+        //     this.$store.state.list.changeNum =  i*i;
+        // },
+            'getData': 'actionItems'
+        }) 
+    } 
 }
 </script>
 <style scoped>
